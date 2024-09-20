f7f1d53ed2a620b022ac33a0b33a65c0c740eeba
FERCHIMS <gr.egfredderick@gmail.com>
2024-09-19 22:55:07 -0300

    esto es lo priemero

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+
+
+export default function Home() {
+  return(
+    <div className="w-full h-[100vh]">
+      <Image src="https://iili.io/dr01Vg1.jpg" width={1000} height={1000} />
+    </div>
+  )
+}
