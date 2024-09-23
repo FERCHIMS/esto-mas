dea2707acc002913b3a2eb7129ab6b2a43a1d11a
FERCHIMS <gr.egfredderick@gmail.com>
2024-09-23 00:09:21 -0300

    tercer intento de commit animacion

--- src/app/jewelry/page.jsx
+++ src/app/jewelry/page.jsx
@@ -8,9 +8,7 @@ const jewelry = () => {
   return (
 
     <div className='flex w-full flex-col overflow-hidden'>
-      <div className=' w-full h-screen'>  
-          <JewelryComp />
-      </div>
+      
 
       <div className='sticky mt-[50vh] z-30 h-screen bg-green-400'>
       <h2 className={`text-[8rem] pl-20 ${gasoekOne.className}` } >ESTO 222222222</h2>
