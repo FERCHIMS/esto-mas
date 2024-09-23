73abbdbce6927f923ac491c1c8e5bc98ba57481e
FERCHIMS <gr.egfredderick@gmail.com>
2024-09-22 23:59:03 -0300

    segundo intento de commit

--- src/components/videosPruebas/VideosPrueba.jsx
+++ src/components/videosPruebas/VideosPrueba.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const VideosPrueba = () => {
   return (
-    <div className=' pt-[30vh]'>
+    <div className=' '>
         <video className='w-full h-screen object-cover' autoPlay muted loop>
             <source  src='https://res.cloudinary.com/dtxyn8cnq/video/upload/v1726841288/4k_video_nzvshz.mp4' />
         </video>
